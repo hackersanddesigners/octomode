87002827e35a4067cfbfbd32a4ce706cd2fae384
systers <info@varia.zone>
2021-06-28 14:11:31 +0200

    This project is an update of the post-scrip interface, now using pagedjs

--- start.py
+++ start.py
@@ -21,10 +21,10 @@ APP = flask.Flask(__name__)
 # 	print('Do something recurrent')
 
 pads = [
-		'post-script.md',
-		'post-script.css'
+		'RDI.md',
+		'RDI.css'
 	]
-DIR_PATH = '/home/mb/post-script-interface'
+DIR_PATH = '/home/systers/rdi-flask-interface'
 
 def download(pads):
 	# using etherpump
@@ -35,25 +35,34 @@ def download(pads):
 def pad():
 	return flask.render_template('pad.html')
 
+@APP.route('/pagedjs/', methods=['GET', 'POST'])
+def pagedjs():
+        # download the main post-script pad + stylesheet pad
+	download(pads)
+	# generate html page
+	os.system(f'pandoc -f markdown -t html -c RDI.css --toc --toc-depth=1 --template { DIR_PATH }/templates/pandoc-template-pagedjs.html --standalone { DIR_PATH }/static/RDI.md -o { DIR_PATH }/static/RDI.pagedjs.html')
+
+	return open('static/RDI.pagedjs.html', 'r').read()
+
 @APP.route('/html/', methods=['GET', 'POST'])
 def html():
 	# download the main post-script pad + stylesheet pad
 	download(pads)
 	# generate html page
-	os.system(f'pandoc -f markdown -t html -c post-script.css --toc --toc-depth=1 --standalone { DIR_PATH }/static/post-script.md -o { DIR_PATH }/static/post-script.html')
+	os.system(f'pandoc -f markdown -t html -c RDI.css --toc --toc-depth=1 --standalone { DIR_PATH }/static/RDI.md -o { DIR_PATH }/static/RDI.html')
 
 	return flask.render_template('html.html')
 
-@APP.route('/pdf/', methods=['GET'])
-def pdf():
+#@APP.route('/pdf/', methods=['GET'])
+#def pdf():
 	# download the main post-script pad + stylesheet pad
-	download(pads)
+	#download(pads)
 	# generate html page
-	os.system(f'pandoc -f markdown -t html -c post-script.css --toc --toc-depth=1 --standalone { DIR_PATH }/static/post-script.md -o { DIR_PATH }/static/post-script.html')
+	#os.system(f'pandoc -f markdown -t html -c post-script.css --toc --toc-depth=1 --standalone { DIR_PATH }/static/post-script.md -o { DIR_PATH }/static/post-script.html')
 	# generate pdf
-	os.system(f'{ DIR_PATH }/venv/bin/weasyprint -s { DIR_PATH }/static/post-script.css { DIR_PATH }/static/post-script.html { DIR_PATH }/static/post-script.pdf')
+	#os.system(f'{ DIR_PATH }/venv/bin/weasyprint -s { DIR_PATH }/static/post-script.css { DIR_PATH }/static/post-script.html { DIR_PATH }/static/post-script.pdf')
 
-	return flask.render_template('pdf.html')
+	#return flask.render_template('pdf.html')
 
 @APP.route('/stylesheet/', methods=['GET'])
 def stylesheet():
@@ -61,4 +70,4 @@ def stylesheet():
 
 if __name__ == '__main__':
 	APP.debug=True
-	APP.run(port=5577)
+	APP.run(port=5588)
