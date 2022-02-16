6660404dec63173c8dc2b30564b1b23500074852
manetta <mail@manettaberends.nl>
2022-02-16 23:18:27 +0100

    adding a default config file, with missing API key

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config(object):
+	PORTNUMBER = 5001
+	PAD_URL = 'https://pad.vvvvvvaria.org/' # with a slash in the end!
+	PAD_API_URL = 'https://pad.vvvvvvaria.org/api/1.2.15/'
+	PAD_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
